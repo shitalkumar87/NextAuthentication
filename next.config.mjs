0c905a6b02ea6a28fa21e8cfbcb6184cff8ab329
shitalkumar87 <shitalsmn@gmail.com>
2024-08-16 22:49:31 +0530

    config env added

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,11 @@
 const nextConfig = {
     eslint:{
         ignoreDuringBuilds: true,
-      }
+      },
+
+      env: {
+        JWT_SECRET: process.env.Token_Secret,
+      },
 };
 
 export default nextConfig;
